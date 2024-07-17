ffb5e10c7aeb450546027420931a46ba492e6e2a
Diego Ramon <brilhocelestebc15@gmail.com>
2024-07-17 02:29:08 -0300

    FEAT: added finalized table in events

--- api/src/main/resources/db/migration/V1__create-event-table.sql
+++ api/src/main/resources/db/migration/V1__create-event-table.sql
@@ -1,11 +1,12 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 CREATE TABLE event (
-    id UUID gen_random_uuid() PRIMARY KEY,
-    title VARCHAR(100) NOT NULL
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
     description VARCHAR(250) NOT NULL,
     img_url VARCHAR(100) NOT NULL,
-    event-url VARCHAR(100) NOT NULL,
+    event_url VARCHAR(100) NOT NULL,
     date TIMESTAMP NOT NULL,
     remote BOOLEAN NOT NULL,
+    finished BOOLEAN NOT NULL
 );
